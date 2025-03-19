9e542c43e54d74f0bac4e6ecbc5e9779b2819981
A.Kalnina <a.kalnina@russianpost.ru>
2025-03-19 14:25:54 +0300

    first commit

--- src/main/java/ru/yandex/praktikum/MainPageYandexScooter.java
+++ src/main/java/ru/yandex/praktikum/MainPageYandexScooter.java
@@ -1,2 +1,48 @@
-package ru.yandex.praktikum;public class MainPageYandexScooter {
+package ru.yandex.praktikum;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MainPageYandexScooter {
+
+    private WebDriver driver;
+
+//Лого в хидере
+    private By yandexLogo = By.xpath(".//a/img[@alt='Yandex']");
+
+//Заголовок "Вопросы о важном"
+    private By questionsHeader = By.xpath(".//div[contains(text(), 'Вопросы о важном')]");
+
+//Список вопросов
+    private By questionsList = By.xpath(".//div[@class = 'accordion__heading']");
+    //private By question = By.xpath(".//div[text() = '"+ text + "']");
+
+//Список ответов
+    private By answer = By.xpath(".//div[@class = 'accordion__heading']/../..//div//p");
+
+
+    // Конструктор класса
+    public MainPageYandexScooter(WebDriver driver){
+        this.driver = driver;
+    }
+
+    // Метод ожидания загрузки страницы
+    public void waitForLoadLogo(){
+        new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.visibilityOfElementLocated(yandexLogo));
+    }
+
+    // Метод, который кликает на вопрос по заданному тексту
+    public void clickQuestionByText(String text){
+        driver.findElement(By.xpath(".//div[text() = '"+ text + "']")).click();
+    }
+
+    // Метод, который проверяет текст ответа
+    public void checkAnswer(String text){
+        driver.findElement(answer).getText();
+    }
+
 }
--- src/test/java/questions/questionsAndAnswers.java
+++ src/test/java/questions/questionsAndAnswers.java
@@ -1,2 +1,37 @@
-package questions;public class questionsAndAnswers {
+package questions;
+
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import ru.yandex.praktikum.BaseSteps;
+import ru.yandex.praktikum.MainPageYandexScooter;
+
+public class questionsAndAnswers {
+
+    private WebDriver driver;
+
+    @Test
+    public void checkAnswersTest() {
+
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        //Закрыть поп-ап с куками
+        BaseSteps baseSteps = new BaseSteps(driver);
+        baseSteps.clickAcceptCookiesButton();
+
+        MainPageYandexScooter mainPage = new MainPageYandexScooter(driver);
+
+        mainPage.clickQuestionByText("Сколько это стоит? И как оплатить?");
+        mainPage.checkAnswer("Сутки — 400 рублей. Оплата курьеру — наличными или картой.");
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
